2fe89327fbc4b34056e1cd610b606e19f8388eff
Jeffrey Scott Flesher <thelightwizzard@gmail.com>
2020-06-30 00:51:53 -0700

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -4,6 +4,6 @@
 # $3 qpm path ("" for no qpm)
 # $4 build mode (i.e. "" or "-native" )
 
-image=skycoder42/qt-build$4
+image=light-wizzard/qt-build$4
 echo Building with image $image
 docker run --rm -e "QPM_PATH=$3" --name docker-qt-build -v "$2:/tmp/qt-build/inst" -e "BUILD_GIT_SRC=$1" $image
